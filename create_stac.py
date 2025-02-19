90bb561761f39182a2cff46f19089c3f8fd752a3
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-02-19 11:26:26 +0100

    Put stac into separate directory

--- create_stac.py
+++ create_stac.py
@@ -1,15 +1,30 @@
-import shutil
-import tempfile
+#!/usr/bin/env python3
+
+#
+# Creates a STAC catalog from a nested dictionary of GEoTIF files
+#
+
 from pathlib import Path
 import pystac
 import rasterio
 from shapely.geometry import Polygon, mapping
-from datetime import datetime as dt
+from datetime import datetime
 
 
 catalog = pystac.Catalog(
     id="rqa-deforestation-test-data",
-    description="RQADeforestation test data. Subset of Sentinel-1 Sigma0.",
+    description="RQADeforestation test data. Subset of Sentinel-1 Sigma0 containing the tile E051N018T3.",
+)
+
+collection = pystac.Collection(
+    id="rqa-deforestation-test-data",
+    description="RQADeforestation test data. Subset of Sentinel-1 Sigma0 containing the tile E051N018T3.",
+    extent=pystac.Extent(
+        spatial=pystac.SpatialExtent([[-180.0, -90.0, 180.0, 90.0]]),
+        temporal=pystac.TemporalExtent(
+            [[datetime(2020, 1, 1), datetime(2022, 12, 31)]]
+        ),
+    ),
 )
 
 
@@ -28,7 +43,7 @@ def to_stac_item(raster_uri):
         tags = ds.tags()
         datetime = dt.fromisoformat(tags["sensing_date"])
         item = pystac.Item(
-            id=raster_uri,
+            id=raster_uri + ".json",
             geometry=mapping(footprint),
             bbox=bbox,
             datetime=datetime,
@@ -37,7 +52,7 @@ def to_stac_item(raster_uri):
         )
         item.add_asset(
             key="image",
-            asset=pystac.Asset(href=ds.files[0], media_type=pystac.MediaType.GEOTIFF),
+            asset=pystac.Asset(href=raster_uri, media_type=pystac.MediaType.GEOTIFF),
         )
         return item
 
@@ -45,7 +60,7 @@ def to_stac_item(raster_uri):
 for path in list(Path(".").rglob("*.tif")):
     raster_uri = str(path)
     item = to_stac_item(raster_uri)
-    catalog.add_item(item)
+    collection.add_item(item)
 
-catalog.set_self_href("catalog.json")
-catalog.save()
+catalog.add_child(collection)
+catalog.normalize_and_save("stac", catalog_type=pystac.CatalogType.SELF_CONTAINED)
